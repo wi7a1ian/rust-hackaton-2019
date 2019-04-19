e67268727723d4f2095746d71286c9f46f72f0d5
Wit <witalian.zajac@gmail.com>
2019-04-19 09:34:55 +0200

    rust project shell

---
+++ fat32lib/src/lib.rs
@@ -0,0 +1,9 @@
+fn kamil_is_a_snake() -> i32
+{
+    let mut a : i32 = 932847;
+    a = 383838;
+    a
+}
+
+#[cfg(test)]
+mod tests;
---
+++ fat32lib/src/tests.rs
@@ -0,0 +1,6 @@
+use super::*;
+
+#[test]
+fn it_works() {
+    assert_eq!(kamil_is_a_snake(), 383838);
+}
