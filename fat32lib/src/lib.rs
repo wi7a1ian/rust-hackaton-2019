d020241d3858da0145b401eee3010b881e0d8eba
Wit <witalian.zajac@gmail.com>
2019-04-19 12:47:10 +0200

    Added sth

--- fat32lib/src/lib.rs
+++ fat32lib/src/lib.rs
@@ -1,6 +1,33 @@
 use std::fs::File;
 use std::io::Read;
 
+struct PartitionTableStruct
+{
+	bootIndicator : u8,         /* Boot flags                 */
+	startingHead : u8,          /* Starting head              */
+    startingSector : u8,        /* Starting sector            */
+	startingCylinder : u8,      /* Starting cylinder          */
+
+	systemID : u8,              /* 2:Xenix, 5:X-DOS, 7:HPFS,  */
+						        /* 99:UNIX, 100-104:NetWare,  */
+							    /* 196-198:DRDOS, 0,1,3,4,6,  */
+						        /* 8,9:DOS,219:Concurrent DOS,*/
+						        /* 80-84:Ontrack, B,C:FAT32   */
+	endingHead : u8,            /* Partition ending head      */
+	endingSector : u8,          /* Partition ending sector    */
+	endingCylinder : u8,        /* Partition ending cylinder  */
+
+    relativeSector : u32,       /* Relative parition addr     */
+	sectorCnt : u32             /* Num sectors in partition   */
+}
+
+struct MbrStruct
+{
+	bootCode : [u8;446],
+	partition : [PartitionTableStruct;4],
+	signature: [u8;2]
+}
+
 fn io_read(path: &str, start_sector: u64, count: u64) -> Vec<u8>
 {
     let mut handle = File::open(path).unwrap();
@@ -12,16 +39,18 @@ fn io_read(path: &str, start_sector: u64, count: u64) -> Vec<u8>
     buffer
 }
 
-struct MbrStruct
-{
-    i : i32,
-}
 
-fn to_struct(v : &Vec<u8>) -> MbrStruct
+fn to_struct<T>(v : &Vec<u8>) -> T
 {
-    let s: MbrStruct = unsafe { std::ptr::read(v.as_ptr() as *const _) };
+    let s: T = unsafe { std::ptr::read(v.as_ptr() as *const _) };
     s
 }
+
+// impl std::fmt::Display for Point {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "({}, {})", self.x, self.y)
+//     }
+// }
 
 #[cfg(test)]
 mod tests;
--- fat32lib/src/tests.rs
+++ fat32lib/src/tests.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use super::*;
 
 #[test]
@@ -19,10 +20,34 @@ fn check_if_mbr_has_valid_signature() {
     assert_eq!(signature[1],expected[1]);
 }
 
+
 #[test]
 fn typecasting_is_working() {
+    struct SomeStruct { i : i32, }
     let mut one_elem_vec : Vec<u8> = vec![5,0,0,0];
-    let mbr = to_struct(&one_elem_vec);
+    let s : SomeStruct = to_struct(&one_elem_vec);
 
-    assert_eq!(5,mbr.i);
+    assert_eq!(5, s.i);
+}
+
+#[test]
+fn check_if_mbr_can_be_casted() {
+    let mut first_sector = io_read(r#"\\.\PHYSICALDRIVE1"#, 0, 1);
+    let expected = [0x55, 0xAA];
+
+    let mbr : MbrStruct = to_struct(&first_sector);
+
+    assert_eq!(mbr.signature[0],expected[0]);
+    assert_eq!(mbr.signature[1],expected[1]);
+}
+
+#[test]
+fn check_if_mbr_has_valid_partition_position() {
+    let mut first_sector = io_read(r#"\\.\PHYSICALDRIVE1"#, 0, 1);
+    
+    let mbr : MbrStruct = to_struct(&first_sector);
+
+    assert_eq!(mem::size_of::<PartitionTableStruct>(), 16 as usize)
+    assert_eq!(mbr.partition[0].sectorCnt, 4_188_160);
+    //assert_eq!(mbr.partition[0].relativeSector, 128);
 }
