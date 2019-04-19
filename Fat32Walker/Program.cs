e997daf6317d57624e29094a65151042597a6593
Wit <witalian.zajac@gmail.com>
2019-04-19 09:03:25 +0200

    project  init

---
+++ Fat32Walker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fat32Walker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
