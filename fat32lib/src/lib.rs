7adcd9fcd5dfe901c920c3d8c326fb52e65e9fbf
Wit <witalian.zajac@gmail.com>
2019-04-19 11:39:50 +0200

    Added working two tests

--- fat32lib/src/lib.rs
+++ fat32lib/src/lib.rs
@@ -1,9 +1,17 @@
-fn kamil_is_a_snake() -> i32
+use std::fs::File;
+use std::io::Read;
+
+fn io_read(path: &str, start_sector: u64, count: u64) -> Vec<u8>
 {
-    let mut a : i32 = 932847;
-    a = 383838;
-    a
+    let mut handle = File::open(path).unwrap();
+
+    let mut buffer = vec![0u8;512*count as usize];
+    //or let mut buffer : Vec<u8> = Vec::with_capacity(512 * count as usize);
+    handle.read_exact(&mut buffer);
+
+    buffer
 }
+
 
 #[cfg(test)]
 mod tests;
--- fat32lib/src/tests.rs
+++ fat32lib/src/tests.rs
@@ -1,6 +1,20 @@
 use super::*;
 
 #[test]
-fn it_works() {
-    assert_eq!(kamil_is_a_snake(), 383838);
+fn io_read_can_read_from_drive() {
+    let first_sector = io_read(r#"\\.\PHYSICALDRIVE1"#, 0, 1);
+    assert_eq!(first_sector.len(),512);
+}
+
+#[test]
+fn if_mbr() {
+    let mut first_sector = io_read(r#"\\.\PHYSICALDRIVE1"#, 0, 1);
+    let mut signature = &first_sector[510..512];
+    let expected = [0x55, 0xAA];
+    for character in signature{
+        print!("{:x?} ", character);
+    }
+
+    assert_eq!(signature[0],expected[0]);
+    assert_eq!(signature[1],expected[1]);
 }
