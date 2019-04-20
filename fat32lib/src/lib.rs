946f7274e6bbd4049be1b4634a7c6b7688eaf34e
salkam3156 <salkam3156@hotmail.com>
2019-04-20 21:22:49 +0200

    to_struct into trait implemented for u8 containers

--- fat32lib/src/lib.rs
+++ fat32lib/src/lib.rs
@@ -1,36 +1,33 @@
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::fs::File;
 use std::io::Read;
 use std::io::SeekFrom;
 
 mod fat32structs;
 pub use fat32structs::*;
 
-fn io_read(path: &str, start_sector: u64, count: u64) -> Vec<u8>
-{
-    const sector_size : usize = 512;
+fn io_read(path: &str, start_sector: u64, count: u64) -> Vec<u8> {
+    const sector_size: usize = 512;
     let mut handle = File::open(path).unwrap();
 
-    let mut buffer = vec![0u8;sector_size * count as usize];
+    let mut buffer = vec![0u8; sector_size * count as usize];
     handle.seek(SeekFrom::Start(sector_size as u64 * start_sector));
     handle.read_exact(&mut buffer);
 
     buffer
 }
 
-
-fn to_struct<T>(v : &Vec<u8>) -> T
-{
-    let s: T = unsafe { std::ptr::read(v.as_ptr() as *const _) };
-    s
+pub trait ToStruct {
+    fn to_struct<T>(&self) -> T;
 }
 
-// impl std::fmt::Display for Point {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         write!(f, "({}, {})", self.x, self.y)
-//     }
-// }
+impl ToStruct for [u8] {
+    fn to_struct<T>(&self) -> T {
+        let s: T = unsafe { std::ptr::read(self.as_ptr() as *const _) };
+        s
+    }
+}
 
 #[cfg(test)]
 mod tests;
